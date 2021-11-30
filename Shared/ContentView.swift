35c39e3d30e116885df9ba13061601743be08935
michaelalvin <smoovemovee@gmail.com>
2021-11-30 14:41:41 -0500

    Added working PieChart components

--- Shared/ContentView.swift
+++ Shared/ContentView.swift
@@ -9,8 +9,7 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        Text("Hello, world!")
-            .padding()
+        PieChartView(values: [1300, 500, 300], colors: [Color.blue, Color.green, Color.orange], names: ["Rent", "Transport", "Education"], backgroundColor: Color(red: 21 / 255, green: 24 / 255, blue: 30 / 255, opacity: 1.0), innerRadiusFraction: 0.6)
     }
 }
 
