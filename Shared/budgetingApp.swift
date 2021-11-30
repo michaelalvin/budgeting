ba1c33869a6cd556d97a6046bbc6a3cfbbdf68ae
Michael Alvin <mchlalvn@umich.edu>
2021-11-30 13:35:02 -0500

    Initial Commit

---
+++ Shared/budgetingApp.swift
@@ -0,0 +1,17 @@
+//
+//  budgetingApp.swift
+//  Shared
+//
+//  Created by Michael Alvin on 11/30/21.
+//
+
+import SwiftUI
+
+@main
+struct budgetingApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
