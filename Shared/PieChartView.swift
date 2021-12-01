f968590bf39f7522406f3e502c3dfc6c217528e2
michaelalvin <smoovemovee@gmail.com>
2021-11-30 21:15:37 -0500

    Reformatted code

--- Shared/PieChartView.swift
+++ Shared/PieChartView.swift
@@ -27,15 +27,15 @@ struct PieChartView: View {
     }
     
     public init(values:[Double], names: [String], formatter: @escaping (Double) -> String, colors: [Color] = [Color.blue, Color.green, Color.orange], backgroundColor: Color = Color(red: 21 / 255, green: 24 / 255, blue: 30 / 255, opacity: 1.0), widthFraction: CGFloat = 0.75, innerRadiusFraction: CGFloat = 0.60){
-            self.values = values
-            self.names = names
-            self.formatter = formatter
-            
-            self.colors = colors
-            self.backgroundColor = backgroundColor
-            self.widthFraction = widthFraction
-            self.innerRadiusFraction = innerRadiusFraction
-        }
+        self.values = values
+        self.names = names
+        self.formatter = formatter
+        
+        self.colors = colors
+        self.backgroundColor = backgroundColor
+        self.widthFraction = widthFraction
+        self.innerRadiusFraction = innerRadiusFraction
+    }
     
     var body: some View {
         GeometryReader { geometry in
@@ -43,47 +43,47 @@ struct PieChartView: View {
                 ZStack{
                     ForEach(0..<self.values.count){ i in
                         PieSliceView(pieSliceData: self.slices[i])
-                                                    .scaleEffect(self.activeIndex == i ? 1.03 : 1)
-                                                    .animation(Animation.spring())
+                            .scaleEffect(self.activeIndex == i ? 1.03 : 1)
+                            .animation(Animation.spring())
                     }
                     .frame(width: widthFraction * geometry.size.width, height: widthFraction * geometry.size.width)
                     .gesture(
-                                            DragGesture(minimumDistance: 0)
-                                                .onChanged { value in
-                                                    let radius = 0.5 * widthFraction * geometry.size.width
-                                                    let diff = CGPoint(x: value.location.x - radius, y: radius - value.location.y)
-                                                    let dist = pow(pow(diff.x, 2.0) + pow(diff.y, 2.0), 0.5)
-                                                    if (dist > radius || dist < radius * innerRadiusFraction) {
-                                                        self.activeIndex = -1
-                                                        return
-                                                    }
-                                                    var radians = Double(atan2(diff.x, diff.y))
-                                                    if (radians < 0) {
-                                                        radians = 2 * Double.pi + radians
-                                                    }
-                                                    
-                                                    for (i, slice) in slices.enumerated() {
-                                                        if (radians < slice.endAngle.radians) {
-                                                            self.activeIndex = i
-                                                            break
-                                                        }
-                                                    }
-                                                }
-                                                .onEnded { value in
-                                                    self.activeIndex = -1
-                                                }
-                                        )
+                        DragGesture(minimumDistance: 0)
+                            .onChanged { value in
+                                let radius = 0.5 * widthFraction * geometry.size.width
+                                let diff = CGPoint(x: value.location.x - radius, y: radius - value.location.y)
+                                let dist = pow(pow(diff.x, 2.0) + pow(diff.y, 2.0), 0.5)
+                                if (dist > radius || dist < radius * innerRadiusFraction) {
+                                    self.activeIndex = -1
+                                    return
+                                }
+                                var radians = Double(atan2(diff.x, diff.y))
+                                if (radians < 0) {
+                                    radians = 2 * Double.pi + radians
+                                }
+                                
+                                for (i, slice) in slices.enumerated() {
+                                    if (radians < slice.endAngle.radians) {
+                                        self.activeIndex = i
+                                        break
+                                    }
+                                }
+                            }
+                            .onEnded { value in
+                                self.activeIndex = -1
+                            }
+                    )
                     Circle()
                         .fill(self.backgroundColor)
                         .frame(width: widthFraction * geometry.size.width * innerRadiusFraction, height: widthFraction * geometry.size.width * innerRadiusFraction)
                     
                     VStack {
-                                            Text(self.activeIndex == -1 ? "Total" : names[self.activeIndex])
-                                                .font(.title)
-                                                .foregroundColor(Color.gray)
-                                            Text(self.formatter(self.activeIndex == -1 ? values.reduce(0, +) : values[self.activeIndex]))
-                                                .font(.title)
-                                        }                }
+                        Text(self.activeIndex == -1 ? "Total" : names[self.activeIndex])
+                            .font(.title)
+                            .foregroundColor(Color.gray)
+                        Text(self.formatter(self.activeIndex == -1 ? values.reduce(0, +) : values[self.activeIndex]))
+                            .font(.title)
+                    }                }
                 PieChartRows(colors: self.colors, names: self.names, values: self.values.map { self.formatter($0) }, percents: self.values.map { String(format: "%.0f%%", $0 * 100 / self.values.reduce(0, +)) })
             }
             .background(self.backgroundColor)
