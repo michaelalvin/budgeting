649507ca69f761d70d20e8c7914c6649fdc9ad03
michaelalvin <smoovemovee@gmail.com>
2021-11-30 21:11:03 -0500

    Added gesture effect

--- Shared/ContentView.swift
+++ Shared/ContentView.swift
@@ -9,7 +9,7 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        PieChartView(values: [1300, 500, 300], colors: [Color.blue, Color.green, Color.orange], names: ["Rent", "Transport", "Education"], backgroundColor: Color(red: 21 / 255, green: 24 / 255, blue: 30 / 255, opacity: 1.0), innerRadiusFraction: 0.6)
+        PieChartView(values: [1300, 500, 300], names: ["Rent", "Transport", "Education"], formatter: {value in String(format: "$%.2f", value)})
     }
 }
 
--- Shared/PieChartView.swift
+++ Shared/PieChartView.swift
@@ -5,8 +5,13 @@ struct PieChartView: View {
     public var colors: [Color]
     public let names: [String]
     
+    public let formatter: (Double) -> String
+    public var widthFraction: CGFloat
+    
     public var backgroundColor: Color
     public var innerRadiusFraction: CGFloat
+    
+    @State private var activeIndex: Int = -1
     
     var slices: [PieSliceData] {
         let sum = values.reduce(0, +)
@@ -21,28 +26,65 @@ struct PieChartView: View {
         return tempSlices
     }
     
+    public init(values:[Double], names: [String], formatter: @escaping (Double) -> String, colors: [Color] = [Color.blue, Color.green, Color.orange], backgroundColor: Color = Color(red: 21 / 255, green: 24 / 255, blue: 30 / 255, opacity: 1.0), widthFraction: CGFloat = 0.75, innerRadiusFraction: CGFloat = 0.60){
+            self.values = values
+            self.names = names
+            self.formatter = formatter
+            
+            self.colors = colors
+            self.backgroundColor = backgroundColor
+            self.widthFraction = widthFraction
+            self.innerRadiusFraction = innerRadiusFraction
+        }
+    
     var body: some View {
         GeometryReader { geometry in
             VStack{
                 ZStack{
                     ForEach(0..<self.values.count){ i in
                         PieSliceView(pieSliceData: self.slices[i])
+                                                    .scaleEffect(self.activeIndex == i ? 1.03 : 1)
+                                                    .animation(Animation.spring())
                     }
-                    .frame(width: geometry.size.width, height: geometry.size.width)
-                    
+                    .frame(width: widthFraction * geometry.size.width, height: widthFraction * geometry.size.width)
+                    .gesture(
+                                            DragGesture(minimumDistance: 0)
+                                                .onChanged { value in
+                                                    let radius = 0.5 * widthFraction * geometry.size.width
+                                                    let diff = CGPoint(x: value.location.x - radius, y: radius - value.location.y)
+                                                    let dist = pow(pow(diff.x, 2.0) + pow(diff.y, 2.0), 0.5)
+                                                    if (dist > radius || dist < radius * innerRadiusFraction) {
+                                                        self.activeIndex = -1
+                                                        return
+                                                    }
+                                                    var radians = Double(atan2(diff.x, diff.y))
+                                                    if (radians < 0) {
+                                                        radians = 2 * Double.pi + radians
+                                                    }
+                                                    
+                                                    for (i, slice) in slices.enumerated() {
+                                                        if (radians < slice.endAngle.radians) {
+                                                            self.activeIndex = i
+                                                            break
+                                                        }
+                                                    }
+                                                }
+                                                .onEnded { value in
+                                                    self.activeIndex = -1
+                                                }
+                                        )
                     Circle()
                         .fill(self.backgroundColor)
-                        .frame(width: geometry.size.width * innerRadiusFraction, height: geometry.size.width * innerRadiusFraction)
+                        .frame(width: widthFraction * geometry.size.width * innerRadiusFraction, height: widthFraction * geometry.size.width * innerRadiusFraction)
                     
                     VStack {
-                        Text("Total")
-                            .font(.title)
-                            .foregroundColor(Color.gray)
-                        Text(String(values.reduce(0, +)))
-                            .font(.title)
-                    }
-                }
-                PieChartRows(colors: self.colors, names: self.names, values: self.values.map { String($0) }, percents: self.values.map { String(format: "%.0f%%", $0 * 100 / self.values.reduce(0, +)) })
+                                            Text(self.activeIndex == -1 ? "Total" : names[self.activeIndex])
+                                                .font(.title)
+                                                .foregroundColor(Color.gray)
+                                            Text(self.formatter(self.activeIndex == -1 ? values.reduce(0, +) : values[self.activeIndex]))
+                                                .font(.title)
+                                        }                }
+                PieChartRows(colors: self.colors, names: self.names, values: self.values.map { self.formatter($0) }, percents: self.values.map { String(format: "%.0f%%", $0 * 100 / self.values.reduce(0, +)) })
             }
             .background(self.backgroundColor)
             .foregroundColor(Color.white)
@@ -78,6 +120,6 @@ struct PieChartRows: View {
 
 struct PieChartView_Previews: PreviewProvider {
     static var previews: some View {
-        PieChartView(values: [1300, 500, 300], colors: [Color.blue, Color.green, Color.orange], names: ["Rent", "Transport", "Education"], backgroundColor: Color(red: 21 / 255, green: 24 / 255, blue: 30 / 255, opacity: 1.0), innerRadiusFraction: 0.6)
+        PieChartView(values: [1300, 500, 300], names: ["Rent", "Transport", "Education"], formatter: {value in String(format: "$%.2f", value)})
     }
 }
