c9e89bbc545894327ed0d9d56c4a45c151ac4fc9
Michael Alvin <smoovemovee@gmail.com>
2021-12-16 22:48:08 -0500

    Wrapped Transaction into TransactionSums struct

--- Shared/TransactionView.swift
+++ Shared/TransactionView.swift
@@ -18,14 +18,12 @@ struct TransactionView: View {
     @State private var notes: String = "Why are you making this purchase? Buy with intent!"
     public var backgroundColor: Color
     
-    @State private var mockExpenses: [Transaction]
     @State private var mockExpenseSums: TransactionSums
     
     public init(backgroundColor: Color = Color(red: 21 / 255, green: 24 / 255, blue: 30 / 255, opacity: 1.0)) {
         self.backgroundColor = backgroundColor
         
-        self.mockExpenses = []
-        self.mockExpenseSums = TransactionSums(values: [Double](repeating: 0.0, count: self.expenseOptions.count), names: self.expenseOptions)
+        self.mockExpenseSums = TransactionSums(values: [Double](repeating: 0.0, count: self.expenseOptions.count), names: self.expenseOptions, transactions: [])
     }
     
     var body: some View {
@@ -55,14 +53,11 @@ struct TransactionView: View {
                     Button(action: {
                         print("Perform an action here...")
                                       
-                        // Update mockExpenses and mockExpensesSums
-                        self.mockExpenses.append(Transaction(name: self.name, amount: self.amount, type: self.expenseOptions[self.expenseIndex], notes: self.notes))
+                        // Update  mockExpensesSums
+                        self.mockExpenseSums.transactions.append(Transaction(name: self.name, amount: self.amount, type: self.expenseOptions[self.expenseIndex], notes: self.notes))
                         
                         self.mockExpenseSums.values[self.expenseIndex] += Double(self.amount) ?? 0.0
-                        
-                        print(self.mockExpenseSums.values)
-                        print("Done updating...")
-                        
+                                                
                         // Clear text fields here
                         self.name = ""
                     }) {
@@ -72,8 +67,8 @@ struct TransactionView: View {
                 
                 List {
                     
-                    ForEach(0..<mockExpenses.count, id: \.self) { Text(self.mockExpenses[$0].name)
-                        Text(self.mockExpenses[$0].type)
+                    ForEach(0..<self.mockExpenseSums.transactions.count, id: \.self) { Text(self.mockExpenseSums.transactions[$0].name)
+                        Text(self.mockExpenseSums.transactions[$0].type)
                     }
                 }
             })
@@ -117,5 +112,6 @@ struct TransactionSums {
 //values: [1300, 500, 300], names: ["Rent", "Transport", "Education"]
     public var values: [Double]
     public var names: [String]
+    public var transactions: [Transaction]
 }
 
